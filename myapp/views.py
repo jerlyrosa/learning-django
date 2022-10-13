61a43cc016a83a3551bc6b0c3312b3e36988bfdb
jerlyrosa <jerlymigueldelarosah@gmail.com>
2022-10-13 15:19:45 -0400

    add admin

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello Vikingos</h1>")
+
+
+def index(request):
+    return HttpResponse("<h1>Index page</h1>")
+
+
+def saludar(request, username):
+
+    return HttpResponse(f'<h1>Hello {username}</h1>')
 
 
 def about(request):
     return HttpResponse("<h1>About Vikingos</h1>")
+
+
+def projects(request):
+    projects = list(Project.objects.values());
+
+    return JsonResponse(projects, safe=False)
+
+
+def tasks(request, id):
+    # task = list(Task.objects.values())
+    # task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse(f"<h1>Task: {task.title}</h1>")
+
